3225e94b415e82a85021c75331d4ba9dc5897cb0
federico gonzalez <fg.cardesign@gmail.com>
2024-01-16 06:59:59 +0100

    preentrega 3 Federico Gonzalez

--- js/index.js
+++ js/index.js
@@ -112,8 +112,121 @@ if (comprarSi === "si"){
 }
 }
 
+// const CONTAINER_CAJA = document.getElementById("sectionContenedor");
 
+// CONTAINER_CAJA.innerHTML = `
+//                         <div class="container caja">
+//                             <div class="row size">
+//                                 <div class="col-lg-4 col-md-6 col-sm-12">
+//                                     <div class="card">
+//                                         <img src="./assets/img/avenida.jpg" class="card-img-top" alt="bicicleta urbana">
+//                                         <div class="cardBody">
+//                                             <h5 class="cardTitle">Bicicleta Urbana</h5>
+//                                             <p class="cardText text-dark">Bicicleta urbana electrica".</p>
+//                                         </div>
+//                                     </div>
+//                                 </div>
+//                                 <div class="col-lg-4 col-md-6 col-sm-12">
+//                                 <div class="card">
+//                                     <img src="./assets/img/d2-city.jpg" alt="bicicleta electrica urbana" class="card-img-top" alt="bicicleta urbana electrica">
+//                                     <div class="cardBody">
+//                                         <h5 class="cardTitle">Urbana Retro</h5>
+//                                         <p class="cardText text-dark">bicicleta electrica urbana retro.</p>
+//                                     </div>
+//                                 </div>
+//                             </div>
+//                             </div>
+//                             <div class="col-lg-4 col-md-6 col-sm-12">
+//                                 <div class="card">
+//                                     <img src="./assets/img/belador-hybrid-ltd-1.jpg" class="card-img-top" alt="bici belador carrera">
+//                                     <div class="cardBody">
+//                                         <h5 class="cardTitle">bicicleta trekking berria</h5>
+//                                         <p class="cardText text-dark">bicicleta de montaña Berria.</p>
+//                                     </div>
+//                                 </div>
+//                             </div>
+//                             <div class="col-lg-4 col-md-6 col-sm-12">
+//                                 <div class="card">
+//                                     <img src="./assets/img/escultura-400.jpg" class="card-img-top" alt="Road bike Merida">
+//                                     <div class="cardBody">
+//                                         <h5 class="cardTitle">Road bike Merida</h5>
+//                                         <p class="cardText text-dark">bicicleta de ruta Merida.</p>
+//                                     </div>
+//                                 </div>
+//                             </div>
+//                             <div class="col-lg-4 col-md-6 col-sm-12">
+//                                 <div class="card">
+//                                     <img src="./assets/img/road-force.jpg" class="card-img-top" alt="Road force merida ">
+//                                     <div class="cardBody">
+//                                         <h5 class="cardTitle">Road Force Merida</h5>
+//                                         <p class="cardText text-dark">Bicicleta Road Force Merida.</p>
+//                                     </div>
+//                                 </div>
+//                             </div>
+//                             <div class="col-lg-4 col-md-6 col-sm-12 size">
+//                                 <div class="card">
+//                                     <img src="./assets/img/corsa.jpg" class="card-img-top" alt="Bicicleta de ciudad corsa">
+//                                     <div class="cardBody">
+//                                         <h5 class="cardTitle">corsa city</h5>
+//                                         <p class="cardText text-dark">Bicicleta de ciudad Corsa.</p>
+//                                     </div>
+//                                 </div>
+//                             </div>
+//                             <div class="col-lg-4 col-md-6 col-sm-12 size">
+//                                 <div class="card">
+//                                     <img src="./assets/img/crossway-l-10-v.jpg" class="card-img-top" alt="Bicicleta de ciudad crossway">
+//                                     <div class="cardBody">
+//                                         <h5 class="cardTitle">Bicicleta Crossway</h5>
+//                                         <p class="cardText text-dark">Bicicleta de ciudad Crossway.</p>
+//                                     </div>
+//                                 </div>
+//                             </div>
+//                             <div class="col-lg-4 col-md-6 col-sm-12 size">
+//                                 <div class="card">
+//                                     <img src="./assets/img/evado-4-0.jpg" class="card-img-top" alt="Bicicleta de ciudad">
+//                                     <div class="cardBody">
+//                                         <h5 class="cardTitle">Bicicleta city</h5>
+//                                         <p class="cardText text-dark">Bicicleta de ciudad.</p>
+//                                     </div>
+//                                 </div>
+//                             </div>
+//                             </div>
+//                         </div>`
+class Producto{
+    constructor(nombre, precio,caracteristicas,img){
+        this.nombre = nombre;
+        this.precio = precio;
+        this.caracteristicas = caracteristicas;
+        this.img = img;
+    }
+}
 
+const PROD1 = new Producto("Bicicleta urbana electrica Devron" ,2750, "Bicicleta urbana", "avenida.jpg");
+const PROD2 = new Producto("Bicicleta urbana electrica Ecobike" ,3215, "Bicicleta urbana", "d2-city.jpg");
+const PROD3 = new Producto("Bicicleta urbana RoadForce" ,4800, "Bicicleta urbana", "road-force.jpg");
+const PROD4 = new Producto("Bicicleta urbana electrica KTM" ,3450, "Bicicleta urbana", "belador-hybrid-ltd-1.jpg");
+const PROD5 = new Producto("Bicicleta ROAD Berria" ,4600, "Bicicleta de ruta", "escultura-400.jpg");
+const PROD6 = new Producto("Bicicleta Road Kross" ,5400, "Bicicleta de ruta", "macina-fun-a510.jpg");
+const PROD7 = new Producto("Bicicleta Road Moser" ,5760, "Bicicleta de ruta", "crossway-l-10-v.jpg");
+const PROD8 = new Producto("Bicicleta urbana Monty" ,2100, "Bicicleta urbana", "evado-4-0.jpg");
+const PROD9 = new Producto("Bicicleta urbana Merida" ,2600, "Bicicleta urbana", "corsa.jpg");
 
+const arrayProductos = [PROD1,PROD2,PROD3,PROD4,PROD5,PROD6,PROD7,PROD8,PROD9];
 
+const contenedorProductos =document.getElementById("contenedorProductos");
 
+arrayProductos.forEach(producto =>{
+    const div = document.createElement("div");
+
+    div.className ="card";
+    div.innerHTML =`
+                <img class="card-img" src="${producto.img}">
+                <div>
+                    <h3>${producto.nombre}</h3>
+                    <h4>${producto.caracteristicas}</h4>
+                    <p>${producto.precio}</p>
+                    <button>comprar</button>
+                </div>
+                `;
+        contenedorProductos.appendChild(div);
+})
